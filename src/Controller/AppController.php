e3b50f7e2c6f6f6b44a43d7adf6076d4ed2f1367
Ambre Kouitini <ambrektn@outlook.fr>
2023-03-22 17:20:37 +0100

    :sparkles: All Routes in App Controler & templates done

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -5,8 +5,9 @@ namespace App\Controller;
 
 
 use App\Repository\DomainRepository;
-
 use App\Repository\StationRepository;
+use App\Repository\SlopeRepository;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,23 +21,50 @@ class AppController extends AbstractController
     public function index(DomainRepository $domainRepository ): Response
     {
         return $this->render('app/index.html.twig', [
-            //'controller_name' => 'AppController',
+            'controller_name' => 'AppController',
             'domains' => $domainRepository->findAll(),
         ]);
     }
 
-    #[Route('/category/{id}', name: 'app_domain')]
-    public function category($id, DomainRepository $domainRepository, StationRepository $stationRepository): Response
+    #[Route('/domain/{id}', name: 'app_domain')]
+    public function domain($id, DomainRepository $domainRepository): Response
     {
         $domain = $domainRepository->find($id);
 
-        if(!$domain){
-            throw $this->createNotFoundException('Category not found');
+        if (!$domain) {
+            throw $this->createNotFoundException('Domain not found');
         }
 
         return $this->render('app/domain.html.twig', [
             'domain' => $domain,
-            'stations' => $stations,
+        ]);
+    }
+
+    #[Route('/station/{id}', name: 'app_station')]
+    public function station($id, StationRepository $stationRepository): Response
+    {
+        $station = $stationRepository->find($id);
+
+        if (!$station) {
+            throw $this->createNotFoundException('Station not found');
+        }
+
+        return $this->render('app/station.html.twig', [
+            'station' => $station,
+        ]);
+    }
+
+    #[Route('/slope/{id}', name: 'app_slope')]
+    public function slope($id, SlopeRepository $slopeRepository): Response
+    {
+        $slope = $slopeRepository->find($id);
+
+        if (!$slope) {
+            throw $this->createNotFoundException('Slope not found');
+        }
+
+        return $this->render('app/slope.html.twig', [
+            'slope' => $slope,
         ]);
     }
 
